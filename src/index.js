612a4369630708bac773798def6f12fd2b862801
zhi.chen <zhi.chen@ucarinc.com>
2017-09-14 15:13:30 +0800

    开发

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function component() {
     var element = document.createElement('div');
     var btn = document.createElement("button");
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello34', 'webpack'], ' ');
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
     element.appendChild(btn);
@@ -13,3 +13,4 @@ function component() {
 }
 
 document.body.appendChild(component());
+
