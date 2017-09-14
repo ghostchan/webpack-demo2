cf1e9a3d3891c24731bbe146000f73d0b1dd922d
zhi.chen <zhi.chen@ucarinc.com>
2017-09-14 16:16:16 +0800

    模块热替换

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js';
-
+import './style.css';
 function component() {
     var element = document.createElement('div');
     var btn = document.createElement("button");
@@ -11,6 +11,16 @@ function component() {
     element.appendChild(btn);
     return element;
 }
+let element = component();
+document.body.appendChild(element);
 
-document.body.appendChild(component());
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log('Accepting the updated printMe module!');
+        // printMe();
+        document.body.removeChild(element);
+        element = component(); // 重新渲染页面后，component 更新 click 事件处理
+        document.body.appendChild(element);
+        })
+}
 
